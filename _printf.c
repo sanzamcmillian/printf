21d0dcae76a5a6c364acf83d7db6288b67ac06d4
sanzamcmillian <g18s2658@ru.ac.za>
2023-07-25 09:04:53 +0200

    Update _printf.c

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	char *round;
+	unsigned int i;
+	char *s
+	int count = 0;
+
+	va_list arg;
+	va_start(arg, format);
+
+	for(round = format; *round != '\0'; round++)
+	{
+		count++;
+		while( *round != '%')
+		{
+			putchar(*round);
+			round++;
+		}
+
+		round++;
+
+		switch(*round)
+		{
+			case 'c' : i = va_arg(arg, int);
+				putchar(i);
+				break;
+			case 's' : s = va_arg(arg, char *);
+				puts(s);
+				break;
+		}
+	}
+	va_end(arg);
+	return (count);
+}
+
+char *convert(unsigned int num, int base)
+{
+	static char representation[] = "0123456789ABCDEF";
+	static char buffer[50];
+	char *ptr;
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do
+	{
+		*--ptr = representation[num%base];
+		num /= base;
+	}while(num != 0);
+
+	return (ptr);
+}
