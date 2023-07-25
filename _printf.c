b951f6b9b842e36a86ef5bac8e4706ec4af44a0d
sanzamcmillian <g18s2658@ru.ac.za>
2023-07-26 00:52:15 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,54 +7,44 @@
  * @format: formats the string used
  * Return: length of the string
  */
+
 int _printf(const char *format, ...)
 {
 	const char *round;
 	unsigned int i;
 	char *s;
 	int count = 0;
-	va_list arg;
 
+	va_list arg;
 	va_start(arg, format);
 
-	for (round = format; *round != '\0'; round++)
+	for(round = format; *round != '\0'; round++)
 	{
-		count++;
-		if (*round != '%')
+		if( *round != '%')
 		{
 			putchar(*round);
+			count++;
 			continue;
 		}
+
 		round++;
-		switch (*round)
+
+		switch(*round)
 		{
-			case 'c':
-				i = va_arg(arg, int);
+			case 'c' : i = va_arg(arg, int);
 				putchar((char)i);
+				count++;
 				break;
-			case 's':
-				s = va_arg(arg, char *);
+			case 's' : s = va_arg(arg, char *);
 				while (*s != '\0')
 				{
 					putchar(*s);
 					s++;
+					count++;
 				}
 				break;
-			case '%':
-				putchar('%');
+			case '%' : putchar('%');
 				break;
-			case 'd':
-				i  = va_arg(arg, int);
-				printf("%d\n", i)
-				break;
-			case 'i':
-				i = va_arg(arg, int);
-				printf("%i\n", i);
-				break;
-			case 'u':
-				i = va_arg(arg, int);
-                                printf("%u\n", i);
-                                break;
 		}
 	}
 	va_end(arg);
